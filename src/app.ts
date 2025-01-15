844179ce33137e30371320c2a2fd34b13480fe0c
--global <--global>
2025-01-15 16:59:21 +0100

    Complete game won and levels logic

--- src/app.ts
+++ src/app.ts
@@ -22,14 +22,25 @@ class GameBoard {
     totalCoins: number;
     coinsThisLevel: number;
     gameover: boolean;
+    num2s3s: number;
 
     constructor() {
         this.level = 1;
         this.totalCoins = 0;
         this.coinsThisLevel = 0;
         this.gameover = false;
+        this.num2s3s = 0;
         this.tiles = this.generateBoard();
         [this.rowSums, this.colSums] = this.calculateRowSums();
+        console.log(this.tiles);
+    }
+
+    resetBoard() {
+        this.coinsThisLevel = 0;
+        this.gameover = false;
+        this.tiles = this.generateBoard();
+        [this.rowSums, this.colSums] = this.calculateRowSums();
+        console.log(this.tiles);
     }
 
     calculateRowSums(): TileSum[][] {
@@ -60,6 +71,7 @@ class GameBoard {
         const [num2s, num3s, numVoltorbs] =
             LEVELS[this.level.toString()][Math.floor(Math.random() * 5)];
         const num1s = TILE_COUNT - num2s - num3s - numVoltorbs;
+        this.num2s3s = num2s + num3s;
         const tiles = [
             ...this.createTileArray(numVoltorbs, 0),
             ...this.createTileArray(num1s, 1),
@@ -88,20 +100,25 @@ class GameBoard {
 
     createTile(value: number): Tile {
         return {
-            isVoltorb: value == 0,
+            isVoltorb: value === 0,
             value: value,
             isRevealed: false,
         };
     }
 
-    renderBoard(board: GameBoard): void {
+    renderGame(): void {
+        this.renderBoard();
+        this.renderInfo();
+    }
+
+    renderBoard() {
         const gameBoard: HTMLElement | null =
             document.getElementById("game-board");
         gameBoard!!.innerHTML = "";
 
-        const tiles = board.tiles;
-        const rowSums = board.rowSums;
-        const colSums = board.colSums;
+        const tiles = this.tiles;
+        const rowSums = this.rowSums;
+        const colSums = this.colSums;
         for (let row = 0; row <= BOARD_SIZE; row++) {
             for (let col = 0; col <= BOARD_SIZE; col++) {
                 const tileElement = document.createElement("div");
@@ -123,14 +140,12 @@ class GameBoard {
                             : tile.value.toString()
                         : "";
                     tileElement.addEventListener("click", () =>
-                        this.clickListener(board, row, col)
+                        this.clickListener(row, col)
                     );
                 }
                 gameBoard?.appendChild(tileElement);
             }
         }
-
-        this.renderInfo();
     }
 
     renderInfo(): void {
@@ -142,40 +157,50 @@ class GameBoard {
         levelCoinsElement!!.textContent = `Coins this level: \n${this.coinsThisLevel}`;
     }
 
-    clickListener(board: GameBoard, row: number, col: number): any {
-        if (board.gameover) {
-            this.coinsThisLevel = 0;
-            this.tiles = this.generateBoard();
-            [this.rowSums, this.colSums] = this.calculateRowSums();
-            this.gameover = false;
-            this.renderBoard(this);
+    clickListener(row: number, col: number): any {
+        if (this.gameover) {
+            this.level--;
+            this.resetBoard();
         } else {
-            const tile = board.tiles[row][col];
+            const tile = this.tiles[row][col];
             tile.isRevealed = true;
             if (tile.isVoltorb) {
-                board.gameover = true;
+                this.gameover = true;
                 alert("Game over!");
-                this.flipBoard(board, true);
+                this.flipBoard(true);
             } else {
+                if (tile.value > 1) {
+                    this.num2s3s--;
+                }
                 this.coinsThisLevel =
                     this.coinsThisLevel > 0
                         ? this.coinsThisLevel * tile.value
                         : tile.value;
             }
-            this.renderBoard(board);
         }
+        if (this.isGameWon()) {
+            this.gameover = true;
+            alert("You won");
+            this.flipBoard(true);
+            this.totalCoins += this.coinsThisLevel;
+            this.level++;
+        }
+        this.renderGame();
     }
 
-    flipBoard(board: GameBoard, reveal: boolean) {
-        const tiles = board.tiles;
+    flipBoard(reveal: boolean) {
+        const tiles = this.tiles;
         tiles.forEach((row, i) => {
             row.forEach((col, i) => {
                 col.isRevealed = reveal;
             });
         });
-        this.renderBoard(board);
+    }
+
+    isGameWon(): boolean {
+        return this.num2s3s === 0;
     }
 }
 
 const g = new GameBoard();
-g.renderBoard(g);
+g.renderGame();
