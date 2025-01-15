114f11edad4638f62a8fb7275cf125eb95926beb
Andreas L <aglgit@users.noreply.github.com>
2025-01-15 10:14:28 +0100

    Add game board logic

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import { LEVELS } from "./LEVELS";
+
 const BOARD_SIZE = 5;
 const TILE_COUNT = BOARD_SIZE * BOARD_SIZE;
 
@@ -7,133 +9,173 @@ type Tile = {
     isRevealed: boolean;
 };
 
-const levels = {
-    "1": [
-        // 2s, 3s, Voltorbs
-        [5, 0, 6],
-        [4, 1, 6],
-        [3, 1, 6],
-        [2, 2, 6],
-        [0, 3, 6],
-    ],
-
-    "2": [
-        [6, 0, 7],
-        [5, 1, 7],
-        [3, 2, 7],
-        [1, 3, 7],
-        [0, 4, 7],
-    ],
-
-    "3": [
-        [7, 0, 8],
-        [6, 1, 8],
-        [4, 2, 8],
-        [2, 3, 8],
-        [1, 4, 8],
-    ],
-
-    "4": [
-        [8, 0, 10],
-        [5, 2, 10],
-        [3, 3, 8],
-        [2, 4, 10],
-        [0, 5, 8],
-    ],
-
-    "5": [
-        [9, 0, 10],
-        [7, 1, 10],
-        [6, 2, 10],
-        [4, 3, 10],
-        [1, 5, 10],
-    ],
-
-    "6": [
-        [8, 1, 10],
-        [5, 3, 10],
-        [3, 4, 10],
-        [2, 5, 10],
-        [0, 6, 10],
-    ],
-
-    "7": [
-        [9, 1, 13],
-        [7, 2, 10],
-        [6, 3, 10],
-        [4, 4, 10],
-        [1, 6, 13],
-    ],
-    "8": [
-        [0, 7, 10],
-        [8, 2, 10],
-        [5, 4, 10],
-        [2, 6, 10],
-        [7, 3, 10],
-    ],
+type TileSum = {
+    sumValue: number;
+    sumVoltorb: number;
 };
 
-function generateBoard(): Tile[][] {
-    const [num2s, num3s, numVoltorbs] = levels[1][3];
-    const num1s = TILE_COUNT - num2s - num3s - numVoltorbs;
-    const tiles = [
-        ...createTileArray(numVoltorbs, 0),
-        ...createTileArray(num1s, 1),
-        ...createTileArray(num2s, 2),
-        ...createTileArray(num3s, 3)
-    ];
+class GameBoard {
+    tiles: Tile[][];
+    rowSums: TileSum[];
+    colSums: TileSum[];
+    level: number;
+    totalCoins: number;
+    coinsThisLevel: number;
+    gameover: boolean;
 
-    for (let i = tiles.length - 1; i >= 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [tiles[i], tiles[j]] = [tiles[j], tiles[i]];
+    constructor() {
+        this.level = 1;
+        this.totalCoins = 0;
+        this.coinsThisLevel = 0;
+        this.gameover = false;
+        this.tiles = this.generateBoard();
+        [this.rowSums, this.colSums] = this.calculateRowSums();
     }
 
-    const grid = [];
-    for (let row = 0; row < BOARD_SIZE; row++) {
-        grid.push(tiles.slice(row * BOARD_SIZE, (row + 1) * BOARD_SIZE));
+    calculateRowSums(): TileSum[][] {
+        const colSums = Array(this.tiles[0].length)
+            .fill(0)
+            .map((v) => {
+                return { sumValue: 0, sumVoltorb: 0 };
+            });
+        const rowSums = Array(this.tiles[0].length)
+            .fill(0)
+            .map((v) => {
+                return { sumValue: 0, sumVoltorb: 0 };
+            });
+        for (let i = 0; i < this.tiles.length; i++) {
+            for (let j = 0; j < this.tiles.length; j++) {
+                rowSums[i].sumValue += this.tiles[i][j].value;
+                colSums[j].sumValue += this.tiles[i][j].value;
+                if (this.tiles[i][j].isVoltorb) {
+                    rowSums[i].sumVoltorb++;
+                    colSums[j].sumVoltorb++;
+                }
+            }
+        }
+        return [rowSums, colSums];
     }
 
-    return grid;
-}
+    generateBoard(): Tile[][] {
+        const [num2s, num3s, numVoltorbs] =
+            LEVELS[this.level.toString()][Math.floor(Math.random() * 5)];
+        const num1s = TILE_COUNT - num2s - num3s - numVoltorbs;
+        const tiles = [
+            ...this.createTileArray(numVoltorbs, 0),
+            ...this.createTileArray(num1s, 1),
+            ...this.createTileArray(num2s, 2),
+            ...this.createTileArray(num3s, 3),
+        ];
 
-function createTileArray(num: number, value: number): Tile[] {
-    return Array(num).fill(value).map(v => createTile(v))
-}
+        for (let i = tiles.length - 1; i >= 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [tiles[i], tiles[j]] = [tiles[j], tiles[i]];
+        }
 
-function createTile(value: number): Tile {
-    return {
-        isVoltorb: value == 0,
-        value: value,
-        isRevealed: true
+        const grid = [];
+        for (let row = 0; row < BOARD_SIZE; row++) {
+            grid.push(tiles.slice(row * BOARD_SIZE, (row + 1) * BOARD_SIZE));
+        }
+
+        return grid;
     }
-}
 
-function renderBoard(board: Tile[][]): void {
-    const gameBoard: HTMLElement | null = document.getElementById("game-board");
-    gameBoard!!.innerHTML = "";
+    createTileArray(num: number, value: number): Tile[] {
+        return Array(num)
+            .fill(value)
+            .map((v) => this.createTile(v));
+    }
 
-    board.forEach((row, i) => {
-        row.forEach((tile, j) => {
-            const tileElement = document.createElement("div");
-            tileElement.className = "tile";
-            tileElement.textContent = tile.isRevealed
-                ? tile.isVoltorb
-                    ? "💣"
-                    : tile.value.toString()
-                : "";
-            tileElement.addEventListener("click", () =>
-                revealTile(board, i, j)
-            );
-            gameBoard?.appendChild(tileElement);
+    createTile(value: number): Tile {
+        return {
+            isVoltorb: value == 0,
+            value: value,
+            isRevealed: false,
+        };
+    }
+
+    renderBoard(board: GameBoard): void {
+        const gameBoard: HTMLElement | null =
+            document.getElementById("game-board");
+        gameBoard!!.innerHTML = "";
+
+        const tiles = board.tiles;
+        const rowSums = board.rowSums;
+        const colSums = board.colSums;
+        for (let row = 0; row <= BOARD_SIZE; row++) {
+            for (let col = 0; col <= BOARD_SIZE; col++) {
+                const tileElement = document.createElement("div");
+                if (row == 5 && col == 5) {
+                    tileElement.className = "empty-tile";
+                } else if (row == 5 || col == 5) {
+                    tileElement.className = "info-tile";
+                    if (row == 5) {
+                        tileElement.textContent = `0${colSums[col].sumValue}\n${colSums[col].sumVoltorb}`;
+                    } else if (col == 5) {
+                        tileElement.textContent = `0${rowSums[row].sumValue}\n${rowSums[row].sumVoltorb}`;
+                    }
+                } else {
+                    tileElement.className = "game-tile";
+                    const tile = tiles[row][col];
+                    tileElement.textContent = tile.isRevealed
+                        ? tile.isVoltorb
+                            ? "💣"
+                            : tile.value.toString()
+                        : "";
+                    tileElement.addEventListener("click", () =>
+                        this.clickListener(board, row, col)
+                    );
+                }
+                gameBoard?.appendChild(tileElement);
+            }
+        }
+
+        this.renderInfo();
+    }
+
+    renderInfo(): void {
+        const levelElement = document.getElementById("info-level");
+        levelElement!!.textContent = `Level: \n${this.level}`;
+        const coinsElement = document.getElementById("info-coins");
+        coinsElement!!.textContent = `Total coins: \n${this.totalCoins}`;
+        const levelCoinsElement = document.getElementById("info-coins-level");
+        levelCoinsElement!!.textContent = `Coins this level: \n${this.coinsThisLevel}`;
+    }
+
+    clickListener(board: GameBoard, row: number, col: number): any {
+        if (board.gameover) {
+            this.coinsThisLevel = 0;
+            this.tiles = this.generateBoard();
+            [this.rowSums, this.colSums] = this.calculateRowSums();
+            this.gameover = false;
+            this.renderBoard(this);
+        } else {
+            const tile = board.tiles[row][col];
+            tile.isRevealed = true;
+            if (tile.isVoltorb) {
+                board.gameover = true;
+                alert("Game over!");
+                this.flipBoard(board, true);
+            } else {
+                this.coinsThisLevel =
+                    this.coinsThisLevel > 0
+                        ? this.coinsThisLevel * tile.value
+                        : tile.value;
+            }
+            this.renderBoard(board);
+        }
+    }
+
+    flipBoard(board: GameBoard, reveal: boolean) {
+        const tiles = board.tiles;
+        tiles.forEach((row, i) => {
+            row.forEach((col, i) => {
+                col.isRevealed = reveal;
+            });
         });
-    });
+        this.renderBoard(board);
+    }
 }
 
-function revealTile(board: Tile[][], i: number, j: number): any {
-    const tile = board[i][j];
-    tile.isRevealed = true;
-    renderBoard(board);
-}
-
-const board = generateBoard();
-renderBoard(board);
+const g = new GameBoard();
+g.renderBoard(g);
