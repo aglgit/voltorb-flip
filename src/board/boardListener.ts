3d06f3362b483cb4ef98e3a7f71fbb66f15029fb
Andreas L <aglgit@users.noreply.github.com>
2025-01-25 11:18:39 +0100

    Move state changes into boardState

--- src/board/boardListener.ts
+++ src/board/boardListener.ts
@@ -1,6 +1,6 @@
 import { Tile } from "@src/types/tiles";
-import { BOARD_SIZE } from "@src/types/constants";
 import BoardState from "./boardState";
+import { Direction } from "../types/direction";
 
 class BoardListener {
     boardState: BoardState;
@@ -10,22 +10,17 @@ class BoardListener {
     }
 
     public gameTileMarkListener(mark: number): void {
-        const tile = this.boardState.selectedTile;
-        this.toggleMark(tile, mark);
+        this.boardState.toggleMark(mark);
     }
 
     public gameTileRevealListener(tile: Tile, i: number, j: number): void {
         if (this.boardState.gameOver) {
             this.boardState.resetBoard();
         } else if (this.boardState.memoMode) {
-            this.boardState.selectedTile = tile;
-            this.boardState.selectedRow = i;
-            this.boardState.selectedCol = j;
+            this.boardState.toggleSelectedTile(tile, i, j);
         } else {
-            this.boardState.selectedTile = tile;
-            this.boardState.selectedRow = i;
-            this.boardState.selectedCol = j;
-            this.revealTile(tile);
+            this.boardState.toggleSelectedTile(tile, i, j);
+            this.boardState.revealTile(tile);
         }
     }
 
@@ -33,43 +28,19 @@ class BoardListener {
         switch (event.key) {
             case "ArrowUp":
                 event.preventDefault();
-                if (this.boardState.selectedRow > 0) {
-                    this.boardState.selectedRow--;
-                    this.boardState.selectedTile =
-                        this.boardState.grid[this.boardState.selectedRow][
-                            this.boardState.selectedCol
-                        ];
-                }
+                this.boardState.moveSelectedTile(Direction.Up);
                 break;
             case "ArrowDown":
                 event.preventDefault();
-                if (this.boardState.selectedRow < BOARD_SIZE - 1) {
-                    this.boardState.selectedRow++;
-                    this.boardState.selectedTile =
-                        this.boardState.grid[this.boardState.selectedRow][
-                            this.boardState.selectedCol
-                        ];
-                }
+                this.boardState.moveSelectedTile(Direction.Down);
                 break;
             case "ArrowLeft":
                 event.preventDefault();
-                if (this.boardState.selectedCol > 0) {
-                    this.boardState.selectedCol--;
-                    this.boardState.selectedTile =
-                        this.boardState.grid[this.boardState.selectedRow][
-                            this.boardState.selectedCol
-                        ];
-                }
+                this.boardState.moveSelectedTile(Direction.Left);
                 break;
             case "ArrowRight":
                 event.preventDefault();
-                if (this.boardState.selectedCol < BOARD_SIZE - 1) {
-                    this.boardState.selectedCol++;
-                    this.boardState.selectedTile =
-                        this.boardState.grid[this.boardState.selectedRow][
-                            this.boardState.selectedCol
-                        ];
-                }
+                this.boardState.moveSelectedTile(Direction.Right);
                 break;
             case "Enter":
                 this.gameTileRevealListener(
@@ -86,69 +57,11 @@ class BoardListener {
             case "1":
             case "2":
             case "3": {
-                const tile = this.boardState.selectedTile;
                 const mark = parseInt(event.key);
-                this.toggleMark(tile, mark);
+                this.boardState.toggleMark(mark);
                 break;
             }
         }
-    }
-
-    private toggleMark(tile: Tile, mark: number): void {
-        if (tile) {
-            if (tile.marks.has(mark)) {
-                tile.marks.delete(mark);
-            } else {
-                tile.marks.add(mark);
-            }
-        }
-    }
-
-    private revealTile(tile: Tile): void {
-        tile.revealed = true;
-        if (tile.voltorb) {
-            this.triggerGameOver();
-        } else {
-            if (tile.value > 1) {
-                this.boardState.num2s3s--;
-            }
-            this.boardState.coinsThisLevel =
-                this.boardState.coinsThisLevel > 0
-                    ? this.boardState.coinsThisLevel * tile.value
-                    : tile.value;
-        }
-        if (this.isGamecomplete()) {
-            this.triggerGameComplete();
-        }
-    }
-
-    private isGamecomplete(): boolean {
-        return this.boardState.num2s3s === 0;
-    }
-
-    private triggerGameOver(): void {
-        this.boardState.gameOver = true;
-        alert("Game over!");
-        this.flipBoard(true);
-        this.boardState.level =
-            this.boardState.level > 1 ? this.boardState.level - 1 : 1;
-    }
-
-    private triggerGameComplete(): void {
-        this.boardState.gameOver = true;
-        alert("You won!");
-        this.flipBoard(true);
-        this.boardState.totalCoins += this.boardState.coinsThisLevel;
-        this.boardState.level++;
-    }
-
-    private flipBoard(reveal: boolean): void {
-        const grid = this.boardState.grid;
-        grid.forEach((row) => {
-            row.forEach((col) => {
-                col.revealed = reveal;
-            });
-        });
     }
 }
 
--- src/board/boardState.ts
+++ src/board/boardState.ts
@@ -3,6 +3,7 @@ import BoardGenerator from "./boardGenerator";
 import { BOARD_SIZE } from "@src/types/constants";
 import { LEVELS, NUM_BOARDS_PER_LEVEL } from "@src/types/levels";
 import { Tile, TileSum } from "@src/types/tiles";
+import { Direction } from "../types/direction";
 
 class BoardState {
     boardGenerator: BoardGenerator;
@@ -67,6 +68,103 @@ class BoardState {
 
     public toggleMemoMode(): void {
         this.memoMode = !this.memoMode;
+    }
+
+    public toggleSelectedTile(tile: Tile, i: number, j: number): void {
+        this.selectedTile = tile;
+        this.selectedRow = i;
+        this.selectedCol = j;
+    }
+
+    public moveSelectedTile(direction: Direction) {
+        switch (direction) {
+            case Direction.Up:
+                if (this.selectedRow > 0) {
+                    this.selectedRow--;
+                    this.selectedTile =
+                        this.grid[this.selectedRow][this.selectedCol];
+                }
+                break;
+            case Direction.Down:
+                if (this.selectedRow < BOARD_SIZE - 1) {
+                    this.selectedRow++;
+                    this.selectedTile =
+                        this.grid[this.selectedRow][this.selectedCol];
+                }
+                break;
+            case Direction.Left:
+                if (this.selectedCol > 0) {
+                    this.selectedCol--;
+                    this.selectedTile =
+                        this.grid[this.selectedRow][this.selectedCol];
+                }
+                break;
+            case Direction.Right:
+                if (this.selectedCol < BOARD_SIZE - 1) {
+                    this.selectedCol++;
+                    this.selectedTile =
+                        this.grid[this.selectedRow][this.selectedCol];
+                }
+                break;
+        }
+    }
+
+    public toggleMark(mark: number): void {
+        const tile = this.selectedTile;
+        if (tile) {
+            if (tile.marks.has(mark)) {
+                tile.marks.delete(mark);
+            } else {
+                tile.marks.add(mark);
+            }
+        }
+    }
+
+    public isGamecomplete(): boolean {
+        return this.num2s3s === 0;
+    }
+
+    public revealTile(tile: Tile): void {
+        tile.revealed = true;
+        if (tile.voltorb) {
+            this.advanceGameOver();
+        } else {
+            if (tile.value > 1) {
+                this.num2s3s--;
+            }
+            this.coinsThisLevel =
+                this.coinsThisLevel > 0
+                    ? this.coinsThisLevel * tile.value
+                    : tile.value;
+        }
+        if (this.isGamecomplete()) {
+            this.advanceGameComplete();
+        }
+    }
+
+    public flipBoard(): void {
+        const grid = this.grid;
+        grid.forEach((row) => {
+            row.forEach((col) => {
+                col.revealed = true;
+            });
+        });
+    }
+
+    private advanceGameOver(): void {
+        // TODO: remove this rendering logic from state
+        alert("Game over!");
+        this.gameOver = true;
+        this.flipBoard();
+        this.level = this.level > 1 ? this.level - 1 : 1;
+    }
+
+    private advanceGameComplete(): void {
+        alert("You won!");
+        this.gameOver = true;
+        this.flipBoard();
+        this.totalCoins += this.coinsThisLevel;
+        this.level++;
     }
 }
 
